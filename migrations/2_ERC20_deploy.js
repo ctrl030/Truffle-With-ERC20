ec8b9235984ec6de03f79f463da38ee81ce58b6b
ctrl030 <70302284+ctrl030@users.noreply.github.com>
2020-11-02 18:36:34 -0300

    3

--- migrations/2_ERC20_deploy.js
+++ migrations/2_ERC20_deploy.js
@@ -1,5 +1,7 @@
 const BananaCoin = artifacts.require("BananaCoin");
 
 module.exports = function(deployer) {
-  deployer.deploy(BananaCoin);
+  deployer.deploy(BananaCoin.deploy).then(function(instance){
+    instance.mint(0xa544a66E7CAB70271D120CB1C4bAe595CCb33fa5, 100)        
+  });
 };
