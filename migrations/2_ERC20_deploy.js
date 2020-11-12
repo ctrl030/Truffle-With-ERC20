0d07f585f4a8cfb429e8156e2157fa96007bf128
ctrl030 <70302284+ctrl030@users.noreply.github.com>
2020-11-12 01:25:48 -0300

    9

--- migrations/2_ERC20_deploy.js
+++ migrations/2_ERC20_deploy.js
@@ -11,4 +11,9 @@ module.exports = async function(deployer, network, accounts) {
   await instance.mint(accounts[1], 100);  
 
   await instance.mint(accounts[2], 100);  
+
+  const balance = await instance.balanceOf(accounts[0]);
+  console.log(accounts[0] + " has the balance of this much Bananas: " + String(balance))
+
+
 };
